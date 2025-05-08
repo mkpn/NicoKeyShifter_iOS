c405e73994e09625484f58c364c876deed250fa1
neesan <exp.ym.lvl@gmail.com>
2025-05-09 01:34:35 +0900

    feat: Factoryライブラリの依存関係を追加

--- Package.swift
+++ Package.swift
@@ -12,14 +12,19 @@ let package = Package(
             targets: ["NicoKeyShifter_iOS"]),
     ],
     dependencies: [
-        // ここに依存関係を追加できます
+        .package(url: "https://github.com/hmlongco/Factory.git", from: "2.4.3")
     ],
     targets: [
         .target(
             name: "NicoKeyShifter_iOS",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Factory", package: "Factory")
+            ]),
         .testTarget(
             name: "NicoKeyShifter_iOSTests",
-            dependencies: ["NicoKeyShifter_iOS"]),
+            dependencies: [
+                "NicoKeyShifter_iOS",
+                .product(name: "FactoryTesting", package: "Factory")
+            ]),
     ]
 ) 
